2762dabf5286c2858a56293dc8addb18c7807a54
FlyingEwok <nicholas.garth@ocsbstudent.ca>
2020-09-17 19:48:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+'''You will design a program that will ask the user to input an x number of elements. Then the program will give an average. Hint( You will want to ask the user to provide the number of elements initially. )'''
+
+#Make a list of user inputed numbers      Note to self create a loop to make user be able to enetr how many nums they want
+xnuminputs = []
+
+#Ask user to enter numbers until they're done
+while True:
+  xnuminputs.append(int(input("Enter a number: ")))
+  i = input("Type done if done otherwise press enter to continue: ")
+  if i == "done":
+    break
+  else:
+    continue
+
+#Take the average of the lists numbers
+Averagexnuminputs = sum(xnuminputs) / len(xnuminputs)
+
+#Print the list and the average to console 
+print("The entered numbers are: ", xnuminputs, "The Average of those numbers is: ", Averagexnuminputs)
