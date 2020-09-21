aa773aa15db5c42503187b32d4f55cb316f97a29
FlyingEwok <71281801+FlyingEwok@users.noreply.github.com>
2020-09-21 17:38:30 -0400

    program

--- main.py
+++ main.py
@@ -17,6 +17,3 @@ Averagexnuminputs = sum(xnuminputs) / len(xnuminputs)
 
 #Print the list and the average to console 
 print("The entered numbers are: ", xnuminputs, "The Average of those numbers is: ", Averagexnuminputs)
-
-print("Hello")
-
